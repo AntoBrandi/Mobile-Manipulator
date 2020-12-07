50c70517ad834835c54d8a0abfada44f5e500945
abrandi@codearchitects.com <abrandi@codearchitects.com>
2020-12-07 14:28:54 +0100

    init

---
+++ mobile_manipulator_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/omncbar/mobile_manipulator_ws/src
+# Build directory: /home/omncbar/mobile_manipulator_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_description")
